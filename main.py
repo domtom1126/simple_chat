f0503245cab3be70b4867cae648eb42130bac3bd
domtom1126 <domtom1126@gmail.com>
2021-05-23 10:11:40 -0700

    Simple chat working

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
+socketio = SocketIO(app)
+
+@app.route('/', methods=['GET', 'POST'])
+def sessions():
+    return render_template('session.html')
+
+def message_receivied(methods=['GET', 'POST']):
+    print('message received')
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET','POST']):
+    print('event received' + str(json))
+    socketio.emit('my response', json, callback=message_receivied)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
+
